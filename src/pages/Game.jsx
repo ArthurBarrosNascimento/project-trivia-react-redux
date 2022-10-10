604b56e4a25e98dd187350bc8a4acda9452bd672
washington gama <wgamaneto16@gmail.com>
2022-10-10 18:34:32 -0300

    req 1-4 pronto

---
+++ src/pages/Game.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Olha o game!
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Game;
